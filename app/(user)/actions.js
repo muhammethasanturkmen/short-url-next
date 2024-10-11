8ee832152784b65748c046392e77db252299e122
muhammet hasan <muhammethasanturkmen@gmail.com>
2024-10-11 16:21:11 +0300

    giris ve cikis yapma eklendi

---
+++ app/(user)/actions.js
@@ -0,0 +1,8 @@
+"use server"
+import { createClient } from "@/utils/supabase/server";
+
+export async function signOut() {
+    const supabase = createClient();
+    const { error } = await supabase.auth.signOut()
+    redirect("/login")
+}
--- app/(user)/layout.jsx
+++ app/(user)/layout.jsx
@@ -1,6 +1,14 @@
-export default function UserLayout({ children }){
+
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { signOut } from "./actions";
+
+export default async function UserLayout({ children }){
+  const supabase = createClient();
+  const {data : { user }, error} = await supabase.auth.getUser();
   return(
       <div>
+          {user ? <form action={signOut}><button>sign out</button></form> : <Link href="/login">login</Link>}
           {children}
       </div>
   )
