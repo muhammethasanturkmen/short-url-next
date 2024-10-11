d3410ad71d174343074b0e265a24ef7b18290699
muhammet hasan <muhammethasanturkmen@gmail.com>
2024-10-11 21:25:12 +0300

    key hatası

--- app/(user)/page.jsx
+++ app/(user)/page.jsx
@@ -31,9 +31,9 @@ export default async function Home() {
         <button className="home-btn" formAction={postLongUrl}>Linki Kısalt</button>
       </form>
       {user ? 
-        registeredUrl.map(x => <div className="urls"><Link className="home-link" href={`/${x.short_url}`}>{x.short_url}</Link></div> )
+        registeredUrl.map((x, i) => <div key={i} className="urls"><Link className="home-link" href={`/${x.short_url}`}>{x.short_url}</Link></div> )
       : 
-        anonUrl.map(x => <div className="urls"><Link className="home-link" href={`/${x.short_url}`}>{x.short_url}</Link></div> )
+        anonUrl.map((x, i) => <div key={i} className="urls"><Link className="home-link" href={`/${x.short_url}`}>{x.short_url}</Link></div> )
       }
     </div>
   );
