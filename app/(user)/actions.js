637e560a866d52621fddbad8e9a291f1d20f0806
muhammet hasan <muhammethasanturkmen@gmail.com>
2024-10-11 17:15:55 +0300

    link kısaltma

--- app/(user)/actions.js
+++ app/(user)/actions.js
@@ -1,8 +1,25 @@
 "use server"
+import { makeid } from "@/utils/link";
 import { createClient } from "@/utils/supabase/server";
 
 export async function signOut() {
     const supabase = createClient();
     const { error } = await supabase.auth.signOut()
     redirect("/login")
+}
+
+export default async function postLongUrl(formData) {
+    const supabase = createClient();
+    const short_url = makeid(6);
+
+    const { error: insertError } = await supabase
+    .from('link')
+    .insert([
+        { 
+            long_url: formData.get("long_url"),
+            short_url: short_url
+        }
+    ]);
+
+    console.log(formData.get("long_url"))
 }
--- app/(user)/page.jsx
+++ app/(user)/page.jsx
@@ -1,7 +1,14 @@
+import postLongUrl from "./actions";
+
 export default async function Home() {
   return (
     <div className="container">
         <h1>link kısaltma sayfası</h1>
+        <form>
+            <input type="text" id="long_url" name="long_url" placeholder="kısaltmak istediğin URL"/>
+
+            <button formAction={postLongUrl}>Linki Kısalt</button>
+        </form>
     </div>
   );
 }
